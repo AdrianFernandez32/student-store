c5d532c802a013241939ee993374b01c4b9ca91f
Adrian Fernandez <adrianfersa@meta.com>
2023-06-20 17:31:42 -0700

    Everything setted up, ready to go

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import * as React from "react"
-import "./Navbar.css"
+import * as React from 'react';
+import './Navbar.css';
+import '../../globals.css';
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <p>Navbar</p>
+    <nav className='navbar'>
+      <p className='text-3xl'>Navbar</p>
     </nav>
-  )
+  );
 }
