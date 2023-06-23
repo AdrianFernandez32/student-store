a07200d2644f3341aa4bd357bfd9b97f253c9a1e
Adrian Fernandez <adrianfersa@meta.com>
2023-06-23 12:03:45 -0700

    Finished cart and taxes

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -6,32 +6,21 @@ import Sidebar from '../Sidebar/Sidebar';
 import Home from '../Home/Home';
 import ProductView from '../Product/ProductView';
 import './App.css';
+import { useToast } from '@chakra-ui/react';
 
 export default function App() {
   const [cart, setCart] = useState(new Map());
   const [subtotal, setSubtotal] = useState(0);
   const [tax, setTax] = useState(0);
   const [total, setTotal] = useState(0);
+  const toast = useToast();
 
   useEffect(() => {
-    // const calculateSubtotal = () => {
-    //   const sub = cart.reduce(
-    //     (accumulator, product) => accumulator + product.price,
-    //     0
-    //   );
-    //   setSubtotal(sub);
-    // };
-    // const calculateTax = () => {
-    //   const taxAmount = subtotal * 0.08;
-    //   setTax(taxAmount);
-    // };
-    // const calculateTotal = () => {
-    //   const totalAmount = subtotal + tax;
-    //   setTotal(totalAmount);
-    // };
-    // calculateSubtotal();
-    // calculateTax();
-    // calculateTotal();
+    const calculateTotal = () => {
+      const totalAmount = subtotal + tax;
+      setTotal(totalAmount);
+    };
+    calculateTotal();
   }, [cart]);
 
   const addToCart = (product) => {
@@ -46,7 +35,8 @@ export default function App() {
       };
       cart.set(product.name, newProduct);
     }
-    console.log(cart);
+    setSubtotal(subtotal + product.price);
+    setTax(tax + product.price * 0.08);
     setCart(new Map(cart));
   };
 
@@ -59,15 +49,36 @@ export default function App() {
         existingProduct.qty--;
         cart.set(product.name, existingProduct);
       }
+      setSubtotal(subtotal - product.price);
+      setTax(tax - product.price * 0.08);
     }
     console.log(cart);
     setCart(new Map(cart));
   };
 
+  const purchase = () => {
+    setCart(new Map());
+    setTax(0);
+    setSubtotal(0);
+    toast({
+      title: 'Purchase completed',
+      description: 'The items will be sent to your address',
+      status: 'success',
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+
   return (
     <div className='app'>
       <BrowserRouter>
-        <Sidebar tax={tax} subtotal={subtotal} total={total} cart={cart} />
+        <Sidebar
+          tax={tax}
+          subtotal={subtotal}
+          total={total}
+          cart={cart}
+          purchase={purchase}
+        />
         <main className='main-content'>
           <Navbar />
           <Routes>
